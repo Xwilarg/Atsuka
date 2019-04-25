cbeebea7767bad00d28f9d81f4cdb97e5011f0ac
Xwilarg <xwilarg@yahoo.fr>
2019-04-25 22:38:35 +0200

    Add censorship system

---
+++ Atsuka/CommunicationModule.cs
@@ -0,0 +1,30 @@
+﻿using Discord;
+using Discord.Commands;
+using DiscordUtils;
+using System;
+using System.Threading.Tasks;
+
+namespace Atsuka
+{
+    public class CommunicationModule : ModuleBase
+    {
+        [Command("Info")]
+        private async Task Info()
+        {
+            await ReplyAsync("", false, Utils.GetBotInfo(Program.P.StartTime, "Atsuki", Program.P.client.CurrentUser));
+        }
+
+        [Command("Help")]
+        private async Task Help()
+        {
+            await ReplyAsync("", false, new EmbedBuilder()
+            {
+                Color = Color.Purple,
+                Title = "Help",
+                Description =
+                    "I'm here to make sure everyone stay polite and civilized." + Environment.NewLine +
+                    "If you want more information about me, you can do the 'info' command"
+            }.Build());
+        }
+    }
+}
--- Atsuka/Program.cs
+++ Atsuka/Program.cs
@@ -1,8 +1,15 @@
 ﻿using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
+using DiscordUtils;
+using Google.Cloud.Translation.V2;
+using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Atsuka
@@ -18,6 +25,22 @@ namespace Atsuka
         public DateTime StartTime { private set; get; }
         public static Program P { private set; get; }
 
+        private string perspectiveApi;
+        private TranslationClient translationClient;
+
+        private List<ulong> bannedIds;
+        private ulong reportGuildId;
+        private ulong reportChanId;
+        private ITextChannel reportChan;
+
+        private static readonly Tuple<string, float>[] categories = new Tuple<string, float>[] {
+            new Tuple<string, float>("SEVERE_TOXICITY", .80f),
+            new Tuple<string, float>("IDENTITY_ATTACK", .80f),
+            new Tuple<string, float>("INSULT", .80f),
+            new Tuple<string, float>("THREAT", .80f),
+            new Tuple<string, float>("OBSCENE", .90f)
+        };
+
         private Program()
         {
             P = this;
@@ -25,15 +48,28 @@ namespace Atsuka
             {
                 LogLevel = LogSeverity.Verbose,
             });
-            client.Log += DiscordUtils.Utils.Log;
-            commands.Log += DiscordUtils.Utils.LogError;
+            client.Log += Utils.Log;
+            commands.Log += Utils.LogError;
+
+            bannedIds = new List<ulong>();
         }
 
         private async Task MainAsync()
         {
             client.MessageReceived += HandleCommandAsync;
 
-            await client.LoginAsync(TokenType.Bot, File.ReadAllText("Keys/token.txt"));
+            await commands.AddModuleAsync<CommunicationModule>(null);
+
+            dynamic json = JsonConvert.DeserializeObject(File.ReadAllText("Keys/keys.json"));
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", (string)json.googleAPIJson);
+            translationClient = TranslationClient.Create();
+            perspectiveApi = json.perspectiveToken;
+
+            reportChan = null;
+            reportChanId = ulong.Parse((string)json.reportChan);
+            reportGuildId = ulong.Parse((string)json.guildId);
+
+            await client.LoginAsync(TokenType.Bot, (string)json.token);
             StartTime = DateTime.Now;
             await client.StartAsync();
 
@@ -45,11 +81,85 @@ namespace Atsuka
             SocketUserMessage msg = arg as SocketUserMessage;
             if (msg == null || arg.Author.IsBot) return;
             int pos = 0;
-            if (msg.HasMentionPrefix(client.CurrentUser, ref pos) || msg.HasStringPrefix("a.", ref pos))
+            if (msg.Channel as ITextChannel == null)
+            {
+                if (msg.Content.Length > 0)
+                {
+                    if (reportChan == null)
+                        reportChan = client.GetGuild(reportGuildId).GetTextChannel(reportChanId);
+                    await reportChan.SendMessageAsync("", false, new EmbedBuilder()
+                    {
+                        Title = "I received a private message from " + arg.Author.ToString(),
+                        Description = msg.Content,
+                        Color = Color.Purple
+                    }.Build());
+                    await msg.Channel.SendMessageAsync("The message you sent me was reported in " + reportChan.Guild.ToString());
+                }
+            }
+            else if (bannedIds.Contains(msg.Author.Id))
+            {
+                if (msg.Content == "I apologize for my bad manners, I promize that I won't do it again")
+                {
+                    bannedIds.Remove(msg.Author.Id);
+                    await msg.Channel.SendMessageAsync("I'm glad you understand.");
+                }
+                else if (await CheckMessage(msg.Content, msg.Author.ToString()))
+                {
+                    try
+                    {
+                        await ((IGuildUser)msg.Author).KickAsync("Speaking rudely");
+                        await msg.Author.SendMessageAsync("It's look like kicking you is the only effective solution." + Environment.NewLine +
+                            "You can come back when you'll be calmed");
+                        bannedIds.Remove(msg.Author.Id);
+                    }
+                    catch (Exception e)
+                    { } // Bot don't have permissions to kick user
+                }
+                else
+                    await msg.DeleteAsync();
+            }
+            else if (await CheckMessage(msg.Content, msg.Author.ToString()))
+            {
+                bannedIds.Add(arg.Author.Id);
+                await msg.Channel.SendMessageAsync("How rude of you, didn't we learn you to speak politely ?" + Environment.NewLine +
+                    "I won't allow you to say anything until you write \"I apologize for my bad manners, I promize that I won't do it again\".");
+            }
+            else if (msg.HasMentionPrefix(client.CurrentUser, ref pos) || msg.HasStringPrefix("a.", ref pos))
             {
                 SocketCommandContext context = new SocketCommandContext(client, msg);
                 await commands.ExecuteAsync(context, pos, null);
             }
+        }
+
+        private async Task<bool> CheckMessage(string content, string username)
+        {
+            if (content.Length == 0)
+                return false;
+            string finalMsg = (await translationClient.TranslateTextAsync(content, "en")).TranslatedText;
+            using (HttpClient hc = new HttpClient())
+            {
+                HttpResponseMessage post = await hc.PostAsync("https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze?key=" + Program.P.perspectiveApi, new StringContent(
+                        JsonConvert.DeserializeObject("{comment: {text: \"" + Utils.EscapeString(finalMsg) + "\"},"
+                                                    + "languages: [\"en\"],"
+                                                    + "requestedAttributes: {" + string.Join(":{}, ", categories.Select(x => x.Item1)) + ":{}} }").ToString(), Encoding.UTF8, "application/json"));
+
+                dynamic json = JsonConvert.DeserializeObject(await post.Content.ReadAsStringAsync());
+                EmbedBuilder embed = new EmbedBuilder()
+                {
+                    Title = "Identification"
+                };
+                List<string> flags = new List<string>();
+                foreach (var s in categories)
+                {
+                    double value = json.attributeScores[s.Item1].summaryScore.value;
+                    if (value >= s.Item2)
+                    {
+                        Console.WriteLine(username + " triggered the flag " + s.Item1 + " with a score of " + s.Item2);
+                        return true;
+                    }
+                }
+            }
+            return false;
         }
     }
 }
